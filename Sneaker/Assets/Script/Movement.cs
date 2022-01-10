0c77eef60a80a748d8f163720b32b10618c7e888
Dalekkai <73823576+Dalekkai@users.noreply.github.com>
2022-01-10 20:19:43 +0100

    VrImplementationRough

--- Sneaker/Assets/Script/Movement.cs
+++ Sneaker/Assets/Script/Movement.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    int speed = 7;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float speed = 10;
+    [SerializeField] float fieldSizeX = 15f;
+    [SerializeField] float fieldSizeZ = 4f;
+    [SerializeField] InputAction input;
+
+
 
     // Update is called once per frame
     void Update()
@@ -17,15 +18,29 @@ public class Movement : MonoBehaviour
         ProcessMovement();
     }
 
-    void ProcessMovement()
-    {   float xThrow = Input.GetAxis("Horizontal");
-        float yThrow = Input.GetAxis("Vertical");
+     void OnEnable() 
+    {
+        input.Enable();
+    }
 
-        float xOffset = xThrow * Time.deltaTime * speed;
-        float yOffset = yThrow * Time.deltaTime * speed;
+    void OnDisable() 
+    {
+        input.Disable();
+
+    }
+
+    void ProcessMovement()
+    {   float xPos  = input.ReadValue<Vector2>().y;
+        float zPos  = -input.ReadValue<Vector2>().x; //Need the joystic y for the Unity Z axis.
+
+        float xOffset = xPos * Time.deltaTime * speed;
+        float zOffset = zPos * Time.deltaTime * speed;
 
         float rawXPos = transform.localPosition.x + xOffset;
-        float rawZPos = transform.localPosition.z + yOffset;
+        float rawZPos = transform.localPosition.z + zOffset;
+
+        float newMovementX = Mathf.Clamp(rawXPos, -fieldSizeX, fieldSizeX); //Futor TODO Clamp the groundsize.
+        float newMovementY = Mathf.Clamp(rawZPos, -fieldSizeZ, fieldSizeZ);
 
          transform.localPosition = new Vector3(rawXPos,transform.localPosition.y, rawZPos);
     }
